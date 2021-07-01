2509dcad4fd085f024827e283a3984c2c2e21a3c
Monaliza Dasig <monalizadasig@gmail.com>
2021-07-01 18:11:09 +0800

    Add event component

---
+++ frontend/src/components/event/EventForm.js
@@ -0,0 +1,243 @@
+import React, { useState, useEffect, useContext } from "react";
+import TextField from "@material-ui/core/TextField";
+import {
+  FormControl,
+  Select,
+  InputLabel,
+  MenuItem,
+  Input,
+  Chip,
+  Button,
+} from "@material-ui/core";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import { makeStyles } from "@material-ui/core/styles";
+import { createEvent, updateEvent } from "./eventService";
+import DatePicker from "react-datepicker";
+import { setHours, setMinutes } from "date-fns";
+import { UsersContext } from "../global/UsersContext";
+import "react-datepicker/dist/react-datepicker.css";
+import "./EventForm.css";
+
+const useStyles = makeStyles((theme) => ({
+  chips: {
+    display: "flex",
+    flexWrap: "wrap",
+  },
+  chip: {
+    margin: 2,
+  },
+}));
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
+
+const EventForm = ({ onCloseModal, onSaveSuccess, selectedEvent }) => {
+  const classes = useStyles();
+  const { usersData } = useContext(UsersContext);
+  const [eventName, setEventName] = useState("");
+  const [selectedUsers, setSelectedUsers] = useState([]);
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  const [error, setError] = useState({});
+
+  useEffect(() => {
+    if (selectedEvent) {
+      const { title, start, end, users: eventUsers } = selectedEvent;
+      const filteredUsers = usersData.filter(
+        (user) => !!eventUsers.find((eventUser) => eventUser.id === user.id)
+      );
+
+      setEventName(title);
+      setStartDate(new Date(start));
+      setEndDate(new Date(end));
+      setSelectedUsers(filteredUsers);
+    }
+  }, [usersData, selectedEvent]);
+
+  const handleEventNameChange = (event) => {
+    setEventName(event.target.value);
+  };
+
+  const handleChange = (event) => {
+    setSelectedUsers(event.target.value);
+  };
+
+  const handleStartChange = (date) => {
+    setStartDate(date);
+  };
+
+  const handleEndChange = (date) => {
+    setEndDate(date);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const validation = validateForm();
+    if (!validation) {
+      return;
+    }
+
+    let usersToAdd = [];
+
+    selectedUsers.forEach((user) => usersToAdd.push(user.id));
+
+    const eventsData = {
+      title: eventName,
+      start: startDate,
+      end: endDate,
+      users: usersToAdd,
+    };
+
+    console.log(eventsData);
+
+    let upsertPromise = null;
+    if (selectedEvent?.id) {
+      upsertPromise = updateEvent(eventsData, selectedEvent?.id);
+    } else {
+      upsertPromise = createEvent(eventsData);
+    }
+
+    try {
+      const result = await upsertPromise;
+      onSaveSuccess(result.data);
+      onCloseModal();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const validateForm = () => {
+    let errors = {};
+    let isValid = true;
+
+    if (eventName === "") {
+      errors.eventName = "This field is required";
+    }
+    if (startDate === null) {
+      errors.startDate = "This field is required";
+    }
+    if (endDate === null) {
+      errors.endDate = "This field is required";
+    }
+
+    if (selectedUsers.length === 0) {
+      errors.selectedUsers = "This field is required";
+    }
+
+    if (Object.keys(errors).length > 0) {
+      isValid = false;
+    }
+
+    setError(errors);
+
+    return isValid;
+  };
+
+  const filterPassedTime = (time) => {
+    const currentDate = new Date();
+    const selectedDate = new Date(time);
+
+    return currentDate.getTime() < selectedDate.getTime();
+  };
+
+  return (
+    <div className='event-form'>
+      <div className='input-group'>
+        <TextField
+          name='eventName'
+          label='Name'
+          type='text'
+          value={eventName}
+          fullWidth
+          onChange={handleEventNameChange}
+          error={error.eventName}
+          helperText={error.eventName}
+        />
+      </div>
+      <div className='input-group'>
+        <label className='date-time-picker-label'>Start</label>
+        <DatePicker
+          selected={startDate}
+          onChange={handleStartChange}
+          showTimeSelect
+          dateFormat='MMMM d, yyyy h:mm aa'
+          className={error.startDate ? "error" : ""}
+          filterTime={filterPassedTime}
+          minTime={setHours(setMinutes(new Date(), 0), 8)}
+          maxTime={setHours(setMinutes(new Date(), 0), 20)}
+        />
+        {error && (
+          <FormHelperText className='error-text'>
+            {error.startDate}
+          </FormHelperText>
+        )}
+      </div>
+      <div className='input-group'>
+        <label className='date-time-picker-label'>End</label>
+        <DatePicker
+          selected={endDate}
+          onChange={handleEndChange}
+          showTimeSelect
+          dateFormat='MMMM d, yyyy h:mm aa'
+          className={error.endDate ? "error" : ""}
+          filterTime={filterPassedTime}
+          minTime={setHours(setMinutes(new Date(), 0), 8)}
+          maxTime={setHours(setMinutes(new Date(), 0), 20)}
+        />
+        {error && (
+          <FormHelperText className='error-text'>
+            {error.endDate}
+          </FormHelperText>
+        )}
+      </div>
+
+      <div className='input-group'>
+        <FormControl variant='outlined' fullWidth error={error.selectedUsers}>
+          <InputLabel id='users'>Users</InputLabel>
+          <Select
+            labelId='users'
+            multiple
+            value={selectedUsers}
+            onChange={handleChange}
+            input={<Input />}
+            renderValue={(selected) => (
+              <div className={classes.chips}>
+                {selected.map((user) => (
+                  <Chip
+                    key={user.id}
+                    label={`${user.firstName} ${user.lastName}`}
+                    className={classes.chip}
+                  />
+                ))}
+              </div>
+            )}
+            MenuProps={MenuProps}
+          >
+            {usersData.map((user) => (
+              <MenuItem key={user.id} value={user}>
+                {`${user.firstName} ${user.lastName}`}
+              </MenuItem>
+            ))}
+          </Select>
+          {error && <FormHelperText>{error.selectedUsers}</FormHelperText>}
+        </FormControl>
+      </div>
+      <div className='form-action'>
+        <Button onClick={handleSubmit} color='primary' variant='contained'>
+          Save
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default EventForm;
---
+++ frontend/src/components/event/Events.js
@@ -0,0 +1,163 @@
+import React, { useState, useEffect } from "react";
+import { Button, DialogActions, Card } from "@material-ui/core";
+import { getEvents, deleteEvent } from "./eventService";
+import Modal from "../Modal";
+import EventForm from "./EventForm";
+import Toast from "../Toast";
+import EventCalendar from "./EventCalendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import "./Events.css";
+
+const Events = () => {
+  const [events, setEvents] = useState([]);
+  const [isOpenModal, setIsOpenModal] = useState(false);
+  const [selectedEvent, setSelectedEvent] = useState(null);
+  const [isConfirmModalOpen, setIsConfirmModalOpen] = useState(false);
+  const [toast, setToast] = useState({ isOpen: false, message: "" });
+
+  const getEventsList = () => {
+    let lists = [];
+
+    events.forEach((event) => {
+      let start = new Date(event.start);
+      let end = new Date(event.end);
+      lists.push({ id: event.id, title: event.title, start, end });
+    });
+
+    return lists;
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const result = await getEvents();
+    setEvents(result.data);
+  };
+
+  const handleOpenModal = () => {
+    setIsOpenModal(true);
+  };
+
+  const closeModal = () => {
+    setIsOpenModal(false);
+    setSelectedEvent(null);
+  };
+
+  const closeConfirmModal = () => {
+    setIsConfirmModalOpen(false);
+    setSelectedEvent(null);
+  };
+
+  const handleEditEvent = (event) => {
+    console.log(event);
+    setSelectedEvent(event);
+    setIsOpenModal(true);
+  };
+
+  const handleDeleteEvent = (event) => {
+    setSelectedEvent(event);
+    setIsConfirmModalOpen(true);
+  };
+
+  const onDeleteEvent = async () => {
+    await deleteEvent(selectedEvent.id);
+    closeConfirmModal();
+    fetchData();
+    showToast("Event successfully deleted!");
+  };
+
+  const showToast = (message) => {
+    setToast({ ...toast, isOpen: true, message });
+  };
+
+  const closeToast = () => {
+    setToast({ ...toast, isOpen: false, message: "" });
+  };
+
+  const handleCreateSuccess = () => {
+    fetchData();
+    showToast("Event successfully added!");
+  };
+
+  const handleUpdateSuccess = () => {
+    fetchData();
+    showToast("Event successfully updated!");
+  };
+
+  return (
+    <div className='events'>
+      <div className='header'>
+        <h3>Events</h3>
+        <button onClick={handleOpenModal} className='button'>
+          + Add Events
+        </button>
+      </div>
+      <div className='events-list'>
+        <h3>List View</h3>
+        {events.map((event) => (
+          <Card key={event.id} className='events-list-item'>
+            {event.title}
+            <div className='events-list-item-actions'>
+              <button
+                style={{ marginRight: 10 }}
+                onClick={() => handleEditEvent(event)}
+                className='button'
+              >
+                Edit
+              </button>
+              <button
+                onClick={() => handleDeleteEvent(event)}
+                className='button'
+              >
+                Delete
+              </button>
+            </div>
+          </Card>
+        ))}
+      </div>
+
+      <div className='calendar'>
+        <h3>Calendar View</h3>
+        <EventCalendar eventsList={getEventsList()} />
+      </div>
+
+      <Toast message={toast.message} open={toast.isOpen} onClose={closeToast} />
+
+      <Modal
+        isOpen={isOpenModal}
+        onClose={closeModal}
+        title={selectedEvent ? "Edit Event" : "Add Event"}
+      >
+        <EventForm
+          onCloseModal={closeModal}
+          onSaveSuccess={
+            selectedEvent ? handleUpdateSuccess : handleCreateSuccess
+          }
+          selectedEvent={selectedEvent}
+        />
+      </Modal>
+
+      <Modal
+        isOpen={isConfirmModalOpen}
+        onClose={closeConfirmModal}
+        title='Delete Event'
+      >
+        <h3 style={{ textAlign: "center" }}>
+          Are you sure you want to delete this event?
+        </h3>
+        <DialogActions>
+          <Button onClick={closeConfirmModal} color='primary'>
+            No
+          </Button>
+          <Button onClick={onDeleteEvent} color='primary'>
+            Yes
+          </Button>
+        </DialogActions>
+      </Modal>
+    </div>
+  );
+};
+
+export default Events;
